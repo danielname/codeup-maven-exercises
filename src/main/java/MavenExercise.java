ac829dcb63d8e5f922d3d824d0a6807104818224
Daniel Name <daniel.v.name@gmail.com>
2022-11-02 10:51:21 -0500

    isnumeric exercise complete

---
+++ src/main/java/MavenExercise.java
@@ -0,0 +1,16 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class MavenExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String userInput = scanner.nextLine();
+        if (StringUtils.isNumeric(userInput)){
+            System.out.printf("%s is a number!%n",userInput);
+        } else {
+            System.out.printf("%s is not a number!%n",userInput);
+        }
+    }
+}
